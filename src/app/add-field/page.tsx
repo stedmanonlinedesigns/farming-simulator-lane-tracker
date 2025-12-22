529da76109fda678b161dd68e74ad1428c2d4496
Michael Stedman <46408716+recondesigns@users.noreply.github.com>
2025-12-22 00:28:24 -0600

    feat(): created add field page

---
+++ src/app/add-field/page.tsx
@@ -0,0 +1,10 @@
+import { Main, PageHeading, PageActions } from "../components"
+
+export default function AddFieldPage() {
+  return (
+    <Main>
+      <PageActions />
+      <PageHeading title="Add field" />
+    </Main>
+  )
+}

8e4aeafde29a45b4293cabd174e46bca75717e28
Michael Stedman <46408716+recondesigns@users.noreply.github.com>
2025-12-28 17:08:41 -0600

    feat(): fixed fetching all trips

--- src/app/trips/page.tsx
+++ src/app/trips/page.tsx
@@ -1,11 +1,21 @@
+"use client"
+import React from "react"
+import { useTripStore } from "@/store/tripStore"
 import { Main, PageHeading, Card } from "../components"
 
 export default function Trips() {
+  const { allTrips, fetchAllTrips } = useTripStore()
+  console.log(555, allTrips)
+
+  React.useEffect(() => {
+    fetchAllTrips()
+  }, [fetchAllTrips])
+
   return (
     <Main>
       <PageHeading title="Trips" />
       <Card>
-        <p>Trips: 0</p>
+        <p>{`Total trips: ${allTrips.length}`}</p>
       </Card>
     </Main>
   )
